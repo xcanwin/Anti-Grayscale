78a46a5b16eb6fa4901ae8812596bc2de983c95a
xcanwin <xcanwin@gmail.com>
2020-04-06 16:14:03 +0800

    增加对!important属性的支持

--- Anti-Grayscale.user.js
+++ Anti-Grayscale.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Anti-Grayscale
 // @namespace    https://github.com/xcanwin/
-// @version      0.3.3
-// @description  移除页面的灰色模式(恢复页面的彩色模式). Remove grayscale of html page (Restore color mode of html page).
+// @version      0.3.4
+// @description  移除页面的灰色模式 (恢复页面的彩色模式). Remove grayscale of html page (Restore color mode of html page).
 // @author       xcanwin
 // @license      MIT
 // @updateURL    https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/master/Anti-Grayscale.user.js
@@ -13,9 +13,13 @@
 
 (function() {
 	'use strict';
+	var filterstyle, re = /grayscale\(.*?\)/gi;
 	["filter", "-webkit-filter", "-moz-filter", "-ms-filter", "-o-filter"].forEach(xcanwin => {
 		Array.prototype.forEach.call(document.getElementsByTagName("*"), function(el) {
-			el.style[xcanwin] = document.defaultView.getComputedStyle(el, null)[xcanwin].replace(/grayscale\(.*?\)/gi, "grayscale(0)");
+			filterstyle = document.defaultView.getComputedStyle(el, null)[xcanwin];
+			if (filterstyle.match(re)){
+				el.style = xcanwin + ": " + filterstyle.replace(re, "grayscale(0) !important");
+			}
 		});
 	});
 })();
