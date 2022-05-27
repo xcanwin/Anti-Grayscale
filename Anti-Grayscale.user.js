0e6d388bc230572648bcaf4a0670aef6d56ae9df
xcanwin <xcanwin@gmail.com>
2022-05-28 00:53:36 +0800

    升级全新的版本

--- Anti-Grayscale.user.js
+++ Anti-Grayscale.user.js
@@ -1,25 +1,34 @@
 // ==UserScript==
 // @name         Anti-Grayscale
-// @namespace    https://github.com/xcanwin/
-// @version      0.3.5
 // @description  移除页面的灰色模式 (恢复页面的彩色模式). Remove grayscale of html page (Restore color mode of html page).
+// @version      1.0
 // @author       xcanwin
-// @license      MIT
-// @updateURL    https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/master/Anti-Grayscale.user.js
+// @namespace    https://github.com/xcanwin/Anti-Grayscale/
 // @supportURL   https://github.com/xcanwin/Anti-Grayscale/
+// @license      AGPL-3.0-only
+// @updateURL    https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/main/Anti-Grayscale.user.js
+// @downloadURL  https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/main/Anti-Grayscale.user.js
+// @run-at       document-end
 // @match        *://*/*
-// @grant        all
+// @grant        none
 // ==/UserScript==
 
 (function() {
-	'use strict';
-	var filterstyle, re = /grayscale\(.*?\)/gi;
-	["filter", "-webkit-filter", "-moz-filter", "-ms-filter", "-o-filter"].forEach(xcanwin => {
-		Array.prototype.forEach.call(document.getElementsByTagName("*"), function(el) {
-			filterstyle = document.defaultView.getComputedStyle(el, null)[xcanwin];
-			if (filterstyle && filterstyle.match(re)) {
-				el.style = xcanwin + ": " + filterstyle.replace(re, "grayscale(0) !important");
-			}
-		});
-	});
+    'use strict';
+
+    let main = function(){
+        Array.prototype.forEach.call(document.getElementsByTagName("*"), function(el) {
+            [
+                "filter", "-webkit-filter", "-moz-filter", "-ms-filter", "-o-filter",
+            ].forEach(xcanwin => {
+                let re = /grayscale\(.*?\)/gi;
+                let style = document.defaultView.getComputedStyle(el, null)[xcanwin];
+                if (style && style.match(re)) {
+                    el.style.setProperty(xcanwin, style.replace(re, "grayscale(0)"), "important");
+                }
+            });
+        });
+    };
+
+    main();
 })();
--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # Anti-Grayscale
 ## 简介
-```
-移除页面的灰色模式 (恢复页面的彩色模式).
-Remove grayscale of html page (Restore color mode of html page).
-```
-
+- 移除页面的灰色模式 (恢复页面的彩色模式).
+- Remove grayscale of html page (Restore color mode of html page).
+- 喜欢这个脚本的小伙伴，可以给我的GITHUB项目 [Anti-Grayscale](https://github.com/xcanwin/Anti-Grayscale/) 点个STAR支持一下
 ## 用法
-1. 安装Tampermonkey插件;
-2. 通过下述三个UserScript源进行安装:
+1. 安装 [Tampermonkey](https://www.tampermonkey.net/) 浏览器插件；
+2. 安装 Anti-Grayscale 脚本，可从下面三个渠道安装：
 
 | 序号 | UserScript源 |
 | --- | --- |
-| 1 | [Github](https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/master/Anti-Grayscale.user.js) |
+| 1 | [Github](https://raw.githubusercontent.com/xcanwin/Anti-Grayscale/main/Anti-Grayscale.user.js) |
 | 2 | [OpenUserJS](https://openuserjs.org/scripts/xcanwin/Anti-Grayscale) |
 | 3 | [GreasyFork](https://greasyfork.org/zh-CN/scripts/399624-anti-grayscale) |
